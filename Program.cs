289f9854ad69a5e2b2e988d37a18b3f88aef5062
zaxanq <zaxanq@gmail.com>
2019-10-06 16:01:45 +0200

    Program 1-1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumaIloczyn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double suma = 0, iloczyn = 1;
+            int n;
+            Console.Write("    Podaj ilość liczb: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"\tPodaj liczbę {i}: ");
+                double number = Convert.ToDouble(Console.ReadLine());
+                suma += number;
+                iloczyn *= number;
+            }
+
+            Console.WriteLine($"\t  Suma tych liczb to: {suma}");
+            Console.WriteLine($"\t  Iloczyn tych licz to: {iloczyn}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Ćwiczenia 1 - Program 1
+28.09.2019
+
+**Program 1-1** przyjmuje liczbę `n` (int). Następnie przyjmuje `n` liczb rzeczywistych (double).
+
+Po podaniu `n` liczb program zwraca sumę tych liczb oraz ich iloczyn.
